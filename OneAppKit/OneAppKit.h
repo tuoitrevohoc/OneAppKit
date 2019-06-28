e4d2e20f5e63c168cf08766f94778ffb27356bf0
Tran Khiem <tuoitrevohoc@me.com>
2019-06-27 22:33:13 -0700

    Initial Commit

---
+++ OneAppKit/OneAppKit.h
@@ -0,0 +1,19 @@
+//
+//  OneAppKit.h
+//  OneAppKit
+//
+//  Created by Tran Thien Khiem on 2019-06-27.
+//  Copyright © 2019 Tran Thien Khiem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OneAppKit.
+FOUNDATION_EXPORT double OneAppKitVersionNumber;
+
+//! Project version string for OneAppKit.
+FOUNDATION_EXPORT const unsigned char OneAppKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneAppKit/PublicHeader.h>
+
+
